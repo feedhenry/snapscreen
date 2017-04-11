1576dcde10e74a4bb5ee51dccc1ad62ef2e2f8a9
Peter Darrow <pmdarrow@gmail.com>
2017-04-11 19:41:10 -0300

    Initial implementation of invite list screen.

---
+++ client-native/screens/InviteDetail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class InviteDetailScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Invite Detail',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Detail Placeholder</Text>
+      </View>
+    );
+  }
+}
