5d37af9ed2074e70a10e6399e69ddcd90058e255
Peter Darrow <pmdarrow@gmail.com>
2017-04-11 19:41:10 -0300

    Initial implementation of invite list screen.

--- client-native/App.js
+++ client-native/App.js
@@ -1,23 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import InviteListScreen from './screens/InviteList';
+import InviteDetailScreen from './screens/InviteDetail';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+export default StackNavigator({
+  // First item in this object is the first screen displayed
+  InvitesList: { screen: InviteListScreen },
+  InviteDetail: { screen: InviteDetailScreen },
 });
