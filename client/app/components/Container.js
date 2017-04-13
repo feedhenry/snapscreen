4774e2d966f5eb7c19a372da6cba68ccf62293b9
damien <damurphy@redhat.com>
2017-04-13 15:53:51 +0100

    adds components for LoginScreen

---
+++ client/app/components/Container.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import { StyleSheet, View } from 'react-native';
+
+const Container = (props) => {
+    return (
+        <View style={styles.labelContainer}>
+            { props.children }
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    labelContainer: {
+        marginBottom: 20
+    }
+});
+
+export default Container;
