c3ae4c72cdbbac7f8a5c8bd439ff06f2cc2e1086
Peter Darrow <pmdarrow@gmail.com>
2017-04-20 01:47:42 -0300

    Ensure native-base fonts are properly loaded on Android.

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,37 @@
+import React from 'react';
+import { Platform } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Font, AppLoading } from 'expo';
+
 import InviteListScreen from './screens/InviteList';
 import InviteDetailScreen from './screens/InviteDetail';
 
-export default StackNavigator({
+const Navigator = StackNavigator({
   // First item in this object is the first screen displayed
   InvitesList: { screen: InviteListScreen },
   InviteDetail: { screen: InviteDetailScreen },
 });
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { isReady: false };
+  }
+  async componentWillMount() {
+    // Prevent font loading errors on Android. Solution copied from
+    // https://github.com/expo/nativebase-example/blob/master/main.js
+    if (Platform.OS === 'android') {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      });
+    }
+    this.setState({ isReady: true });
+  }
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+    return <Navigator />;
+  }
+}
