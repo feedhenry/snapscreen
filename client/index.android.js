b5d6208275c325a32335b582827a03ce7964f016
Summers Pittman <secondsun@gmail.com>
2017-04-25 15:07:56 -0400

    Push Support
    This commit allows users to define push handers by listening to the onDefaultMessage DeviceEvent or by registering a callback listener using native methods

--- client/index.android.js
+++ client/index.android.js
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, DeviceEventEmitter } from 'react-native';
+import { Event } from 'react';
 import App from './app/main';
+
+DeviceEventEmitter.addListener('onDefaultMessage', function(event) {
+    //Replace this with your own handling code.
+    console.log("You have receieved a push message." + JSON.stringify(event));
+  });
 
 AppRegistry.registerComponent('SnapScreen', () => App);
