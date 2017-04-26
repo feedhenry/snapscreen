ae62dbe5d813ee1b668360ebdd035a918425ad63
Emilio Rodriguez <emiliorodriguez@gmail.com>
2017-04-26 20:08:50 +0200

    added cinemas and re-written movies list calls

--- server/api/controllers/movieController.js
+++ server/api/controllers/movieController.js
@@ -1,14 +1,63 @@
 'use strict';
 
-var OnconnectAPI = require('onconnect-movies-api');
-OnconnectAPI.init('gsje5sn4pu563gsrz24pr8zf');
+var CinepassAPI = require('cinepass-api');
+CinepassAPI.init('dh0xy0m0LQZXqvLVMscUWA70jAeRpyLx');
 
-exports.listMoviesNearby = function(req, res) {
-    OnconnectAPI.getMoviesByLocation(req.query.lat, req.query.lng, function(movies) {
-        if(movies.error) {
-        	res.status(500).send(movies);
+exports.listCinemasByLocation = function(req, res) {
+    var location = ''
+    if(req.query.lat && req.query.lng) {
+    	location = req.query.lat + ',' + req.query.lng;
+    } else {
+    	res.status(500).send({error: true, errorMessage: 'lat and lng are required parameters'});
+    	return;
+    }
+
+    CinepassAPI.getCinemas({ location: location }, function(cinemas) {
+        if(cinemas.error) {
+        	res.status(500).send(cinemas);
         	return;
         }
-        res.json(movies);
+        res.json(cinemas);
+    });
+};
+
+exports.listMoviesByCinema = function(req, res) {
+    var cinemaId = ''
+    if(req.query.cinema_id) {
+    	cinemaId = req.query.cinema_id;
+    } else {
+    	res.status(500).send({error: true, errorMessage: 'cinema_id is a required parameters'});
+    	return;
+    }
+
+    CinepassAPI.getShowTimes({ cinema_id: cinemaId }, function(showtimes) {
+    	if(showtimes.error) {
+        	res.status(500).send(showtimes);
+        	return;
+        }
+
+        var showTimesByMovie = {};
+        showtimes.forEach(function(showTime){
+        	if(showTimesByMovie[showTime.movie_id]) {
+        		showTimesByMovie[showTime.movie_id].push(showTime);
+        	} else {
+        		showTimesByMovie[showTime.movie_id] = [showTime];
+        	}
+        });
+
+    	CinepassAPI.getMovies({ cinema_id: cinemaId }, function(movies) {
+	        if(movies.error) {
+	        	res.status(500).send(movies);
+	        	return;
+	        }
+
+	        res.json(movies.map(function(movie){
+	        	return {
+	        		id: movie.id,
+	        		title: movie.title,
+	        		showtimes: showTimesByMovie[movie.id]
+	        	}
+	        }));
+	    });
     });
 };
