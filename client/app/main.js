8d0dbec47628bf0d1341dc56f84a2ec0c2c73e6b
Summers Pittman <secondsun@gmail.com>
2017-04-26 01:24:39 -0400

    KeyCloak Integration

--- client/app/main.js
+++ client/app/main.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Platform, NativeModules } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
+import LoadingScreen from './screens/Loading';
+import LoginScreen from './screens/Login';
 import InviteListScreen from './screens/InviteList';
 import InviteDetailScreen from './screens/InviteDetail';
 import InviteCreateScreen from './screens/InviteCreate';
 
 const Navigator = StackNavigator({
   // First item in this object is the first screen displayed
+  Loading: { screen: LoadingScreen },
+  Login: { screen: LoginScreen },
   InvitesList: { screen: InviteListScreen },
   InviteDetail: { screen: InviteDetailScreen },
   InviteCreate: { screen: InviteCreateScreen },
