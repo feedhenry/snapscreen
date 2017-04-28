5a579ef6b6e5e6f1e36b0b62336807ec3d90f33d
Emilio Rodriguez <emiliorodriguez@gmail.com>
2017-04-28 19:59:07 +0200

    added jwt decoding/encoding

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.keycloak = new Keycloak({});
 
 app.use(app.keycloak.middleware());
+app.use(require('./middleware/jwtDecoder').jwtDecoder);
 app.use(app.keycloak.middleware( { logout: '/'} ));
 
 // endpoint tests
