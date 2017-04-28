290b84d9a04838c68b8617d3001f2bdcc585f81d
Peter Darrow <pmdarrow@gmail.com>
2017-04-28 11:22:48 -0300

    Implement date picker for showtime selection screen.

--- client/app/screens/SelectMovieShowtime.js
+++ client/app/screens/SelectMovieShowtime.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, Image, FlatList } from 'react-native';
+import { ActivityIndicator, Image, FlatList, Picker } from 'react-native';
 import {
   Body,
   Button,
@@ -44,12 +44,28 @@ export default class SelectMovieShowtimeScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedDate: Moment().utc().startOf('day').format(),
+    };
     this.theaterID = this.props.navigation.state.params.theaterID;
     this.onSelect = this.props.navigation.state.params.onSelect;
 
     // Initialize list with movies showing today
-    getMovieShowtimes(this.theaterID, Moment().utc().format())
+    getMovieShowtimes(this.theaterID, this.state.selectedDate)
+      .then(movieShowtimes => {
+        this.setState({ movieShowtimes });
+      })
+      .catch(error => {
+        alert('Error getting movie showtimes: ' + JSON.stringify(error));
+      });
+  }
+
+  _handleDateChange(date) {
+    this.setState({
+      selectedDate: date,
+      movieShowtimes: null,
+    });
+    getMovieShowtimes(this.theaterID, this.state.selectedDate)
       .then(movieShowtimes => {
         this.setState({ movieShowtimes });
       })
@@ -61,6 +77,20 @@ export default class SelectMovieShowtimeScreen extends React.Component {
   _handleShowtimeClick(movie, showtime) {
     this.onSelect({ movie, showtime });
     this.props.navigation.goBack();
+  }
+
+  _getDatesList() {
+    // Get an array of the next 7 days, suitable for displaying in a picker
+    let dates = [];
+    let day = Moment();
+    for (let i = 0; i < 7; i++) {
+      dates.push({
+        label: day.format('dddd, MMMM D'),
+        value: day.utc().startOf('day').format(),
+      });
+      day.add(1, 'days');
+    }
+    return dates;
   }
 
   _renderRow({ item }) {
@@ -98,29 +128,39 @@ export default class SelectMovieShowtimeScreen extends React.Component {
   }
 
   render() {
-    if (this.state.movieShowtimes) {
-      return (
-        <Container>
-          <Content style={styles.content}>
-            <FlatList
-              data={this.state.movieShowtimes}
-              renderItem={this._renderRow.bind(this)}
-            />
-          </Content>
-        </Container>
-      );
-    } else {
-      return (
-        <Container>
-          <Content>
-            <ActivityIndicator
-              animating={this.state.animating}
-              style={[styles.centering, { height: 80 }]}
-              size="large"
-            />
-          </Content>
-        </Container>
-      );
-    }
+    return (
+      <Container>
+        <Content style={styles.content}>
+          <Picker
+            selectedValue={this.state.selectedDate}
+            onValueChange={this._handleDateChange.bind(this)}
+          >
+            <For each="date" index="idx" of={this._getDatesList()}>
+              <Picker.Item
+                label={date.label}
+                value={date.value}
+                key={date.value}
+              />
+            </For>
+          </Picker>
+
+          <Choose>
+            <When condition={this.state.movieShowtimes}>
+              <FlatList
+                data={this.state.movieShowtimes}
+                renderItem={this._renderRow.bind(this)}
+              />
+            </When>
+            <Otherwise>
+              <ActivityIndicator
+                animating={this.state.animating}
+                style={[styles.centering, { height: 80 }]}
+                size="large"
+              />
+            </Otherwise>
+          </Choose>
+        </Content>
+      </Container>
+    );
   }
 }
