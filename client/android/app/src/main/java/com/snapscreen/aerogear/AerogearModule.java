7bbab699de05ad3ee3d1e4ecdfe2f8b3ec84905c
Summers Pittman <secondsun@gmail.com>
2017-05-01 12:54:10 -0400

    Adding Android Source code Formatter.\n Use ./gradlew spotlessApply to run

--- client/android/app/src/main/java/com/snapscreen/aerogear/AerogearModule.java
+++ client/android/app/src/main/java/com/snapscreen/aerogear/AerogearModule.java
@@ -3,90 +3,87 @@ package com.snapscreen.aerogear;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-
-import com.facebook.react.bridge.Callback;
 import com.snapscreen.BuildConfig;
-
+import java.net.URI;
 import org.jboss.aerogear.android.unifiedpush.MessageHandler;
 import org.jboss.aerogear.android.unifiedpush.PushRegistrar;
 import org.jboss.aerogear.android.unifiedpush.RegistrarManager;
 import org.jboss.aerogear.android.unifiedpush.fcm.AeroGearFCMPushConfiguration;
 
-import java.net.URI;
-
 public class AerogearModule extends ReactContextBaseJavaModule {
 
-    private final String VARIANT_ID       = BuildConfig.UNIFIED_PUSH_VARIANT_ID;
-    private final String SECRET           = BuildConfig.UNIFIED_PUSH_SECRET;
-    private final String GCM_SENDER_ID    = BuildConfig.UNIFIED_PUSH_GCM_SENDER_ID;
-    private final String UNIFIED_PUSH_URL = BuildConfig.UNIFIED_PUSH_URL;
+  private final String VARIANT_ID = BuildConfig.UNIFIED_PUSH_VARIANT_ID;
+  private final String SECRET = BuildConfig.UNIFIED_PUSH_SECRET;
+  private final String GCM_SENDER_ID = BuildConfig.UNIFIED_PUSH_GCM_SENDER_ID;
+  private final String UNIFIED_PUSH_URL = BuildConfig.UNIFIED_PUSH_URL;
 
-    public AerogearModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-    }
+  public AerogearModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
 
-    private ReactMessageHandler messageHandler = new ReactMessageHandler();
+  private ReactMessageHandler messageHandler = new ReactMessageHandler();
+
+  @Override
+  public String getName() {
+    return "Aerogear";
+  }
+
+  @ReactMethod
+  public void init(
+      ReadableMap config, final Callback successCallback, final Callback cancelCallback) {
+
+    RegistrarManager.config("register", AeroGearFCMPushConfiguration.class)
+        .setPushServerURI(URI.create(UNIFIED_PUSH_URL))
+        .setSenderId(GCM_SENDER_ID)
+        .setVariantID(VARIANT_ID)
+        .setSecret(SECRET)
+        .setAlias(config.getString("alias"))
+        .asRegistrar();
+
+    PushRegistrar registrar = RegistrarManager.getRegistrar("register");
+    registrar.register(
+        getCurrentActivity().getApplicationContext(),
+        new org.jboss.aerogear.android.core.Callback<Void>() {
+          @Override
+          public void onSuccess(Void data) {
+            successCallback.invoke();
+          }
+
+          @Override
+          public void onFailure(Exception exception) {
+            Log.e("REGISTRATION", exception.getMessage(), exception);
+            cancelCallback.invoke(exception.getMessage());
+          }
+        });
+  }
+
+  @ReactMethod
+  public void registerMessageHandler(Callback messageCallback) {
+    messageHandler.toCall = messageCallback;
+    RegistrarManager.registerMainThreadHandler(messageHandler);
+  }
+
+  @ReactMethod
+  public void unregisterMessageHandler(Callback messageCallback) {
+    messageHandler.toCall = null;
+    RegistrarManager.unregisterMainThreadHandler(messageHandler);
+  }
+
+  private static class ReactMessageHandler implements MessageHandler {
+
+    Callback toCall;
 
     @Override
-    public String getName() {
-        return "Aerogear";
+    public synchronized void onMessage(Context context, Bundle message) {
+      if (toCall != null) {
+        toCall.invoke(message.getString("alert"));
+        toCall = null;
+      }
     }
-
-    @ReactMethod
-    public void init(ReadableMap config, final Callback successCallback, final Callback cancelCallback) {
-
-        RegistrarManager.config("register", AeroGearFCMPushConfiguration.class)
-                    .setPushServerURI(URI.create(UNIFIED_PUSH_URL))
-                    .setSenderId(GCM_SENDER_ID)
-                    .setVariantID(VARIANT_ID)
-                    .setSecret(SECRET)
-                    .setAlias(config.getString("alias"))
-                    .asRegistrar();
-
-        PushRegistrar registrar = RegistrarManager.getRegistrar("register");
-        registrar.register(getCurrentActivity().getApplicationContext(), new org.jboss.aerogear.android.core.Callback<Void>() {
-            @Override
-            public void onSuccess(Void data) {
-                successCallback.invoke();
-            }
-
-            @Override
-            public void onFailure(Exception exception) {
-                Log.e("REGISTRATION", exception.getMessage(), exception);
-                cancelCallback.invoke(exception.getMessage());
-            }
-        });
-    }
-
-    @ReactMethod
-    public void registerMessageHandler(Callback messageCallback) {
-        messageHandler.toCall = messageCallback;
-        RegistrarManager.registerMainThreadHandler(messageHandler);
-    }
-
-    @ReactMethod
-    public void unregisterMessageHandler(Callback messageCallback) {
-        messageHandler.toCall = null;
-        RegistrarManager.unregisterMainThreadHandler(messageHandler);
-    }
-
-    private static class ReactMessageHandler implements MessageHandler{
-
-        Callback toCall;
-
-        @Override
-        public synchronized void onMessage(Context context, Bundle message) {
-            if (toCall != null) {
-                toCall.invoke(message.getString("alert"));
-                toCall = null;
-            }
-        }
-    }
-
-
+  }
 }
--- client/android/app/src/main/java/com/snapscreen/aerogear/AerogearPackage.java
+++ client/android/app/src/main/java/com/snapscreen/aerogear/AerogearPackage.java
@@ -5,7 +5,6 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,13 +22,11 @@ public class AerogearPackage implements ReactPackage {
   }
 
   @Override
-  public List<NativeModule> createNativeModules(
-                              ReactApplicationContext reactContext) {
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
     List<NativeModule> modules = new ArrayList<>();
 
     modules.add(new AerogearModule(reactContext));
 
     return modules;
   }
-
 }
